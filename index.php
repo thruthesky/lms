dec703fd630985edf8c3db2d848417234fcc5476
thruthesky <thruthesky@gmail.com>
2016-03-17 20:20:20 +0900

    adding files on lms

---
+++ index.php
@@ -0,0 +1,18 @@
+<style>
+    .siteapi-index iframe {
+        width:100%;
+        min-height:40960px;
+    }
+</style>
+<div class="wrap plugin-lms-ndex">
+
+    <h2>LMS 0.0.1</h2>
+
+    <div>
+        LMS Manual
+        [ <a href="https://docs.google.com/document/d/1QWzjPc6p-49-tFtlejJmyGrrln5B5u_GBPie7M_DTRM/pub" target=_blank>Open in new window.</a> ]
+    </div>
+
+    <iframe src="https://docs.google.com/document/d/1QWzjPc6p-49-tFtlejJmyGrrln5B5u_GBPie7M_DTRM/pub?embedded=true"></iframe>
+
+</div>
---
+++ lms.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name: LMS
+ * Plugin URI: http://dev.phlgo.com
+ * Author: JaeHo Song
+ * Description: Learning Management System.
+ * Version: 0.0.2
+ */
+include plugin_dir_path(__FILE__) . '/wp-include/library.php';
+
+
+add_action( 'admin_init', function() {
+    register_setting( 'lms', 'lms' );
+});
+add_action( 'wp_before_admin_bar_render', function () {
+    global $wp_admin_bar; // 관리자 툴바
+    $wp_admin_bar->add_menu( array(
+        'id' => 'siteapi_toolbar',
+        'title' => 'LMS',
+        'href' => home_url('wp-admin/admin.php?page=lms%2Findex.php')
+    ) );
+});
+add_action('admin_menu', function () {
+    add_menu_page(
+        'LMS Index',
+        'LMS 0.0.1',
+        'manage_options',
+        'lms/index.php',
+        '',
+        plugin_dir_url( __FILE__ ) . 'icon/lms.png', // 메뉴 아이콘
+        '23.45'
+    );
+    add_submenu_page(
+        'lms/index.php',
+        'LMS Settings',
+        'Settings',
+        'manage_options',
+        'lms/setting.php',
+        ''
+    );
+} );
+
