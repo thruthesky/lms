d5f9643bce39e311b667d59f1e1dae0335c926b6
thruthesky <thruthesky@gmail.com>
2016-03-28 19:10:03 +0900

    updaing for abc

--- lms.php
+++ lms.php
@@ -6,63 +6,21 @@
  * Description: Learning Management System.
  * Version: 0.0.2
  */
+
+if ( ! defined('ABC_LIBRARY') ) {
+    echo "Activate ABC-Library";
+    return;
+}
 /**
  * Defines
  */
 define('LMS_OPTION', 'lms');
-if ( ! defined('WP_LIBRARY') ) die("Activate WP-Library");
 
 /**
  * hooks
  */
 include plugin_dir_path(__FILE__) . 'hook-admin-menu.php';
 include plugin_dir_path(__FILE__) . 'hook-init.php';
-
-
-function lms_init() {
-    load_plugin_textdomain( 'lms', FALSE, basename( dirname( __FILE__ ) ) );
-}
-add_action( 'plugins_loaded', 'lms_init' );
-
-
-add_action( 'admin_init', function() {
-    register_setting( 'lms', 'lms' );
-});
-add_action( 'wp_before_admin_bar_render', function () {
-    global $wp_admin_bar; // 관리자 툴바
-    $wp_admin_bar->add_menu( array(
-        'id' => 'siteapi_toolbar',
-        'title' => 'LMS',
-        'href' => home_url('wp-admin/admin.php?page=lms%2Findex.php')
-    ) );
-});
-add_action('admin_menu', function () {
-    add_menu_page(
-        __('LMS Index', 'lms'),
-        __('LMS 0.0.1', 'lms'),
-        'manage_options',
-        'lms/index.php',
-        '',
-        plugin_dir_url( __FILE__ ) . 'icon/lms.png', // 메뉴 아이콘
-        '23.45'
-    );
-    add_submenu_page(
-        'lms/index.php',
-        __('LMS Settings', 'lms'),
-        __('Settings', 'lms'),
-        'manage_options',
-        'lms/setting.php',
-        ''
-    );
-} );
-
-
-add_action('wp_head', function() {
-    option('lms', 'html_head', false);
-});
-add_action('wp_footer', function() {
-    option('lms', 'html_bottom', false);
-});
 
 
 // 회원 가입 페이지에 양식을 추가
@@ -86,5 +44,4 @@ add_action( 'user_register', function ( $user_id ) {
     update_user_meta($user_id, 'skype', $_POST['skype']);
     update_user_meta($user_id, 'kakao', $_POST['kakao']);
     update_user_meta($user_id, 'address', $_POST['address']);
-
 }, 10, 1);
