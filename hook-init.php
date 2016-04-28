deca3f8f98f4353a17f11f915d365f9c0894d2b8
thruthesky <thruthesky@gmail.com>
2016-04-28 15:23:21 +0800

    updating init

--- hook-init.php
+++ hook-init.php
@@ -7,3 +7,15 @@ function lms_init() {
 add_action( 'plugins_loaded', 'lms_init' );
 display_option_on('wp_head', LMS_OPTION, 'html_head');
 display_option_on('wp_footer', LMS_OPTION, 'html_bottom');
+
+add_action('wp_footer', function(){
+    $domain = get_opt('lms[domain]');
+    if ( empty($domain) ) {
+        echo "
+    <script>
+    alert('Error: No domain in LMS Settings.');
+    </script>
+    ";
+    }
+});
+
