d7159ba3df9d5b6be648ad9eaac01f403bd0291e
lancelynyrd <you@example.com>
2016-06-17 22:25:11 +0800

    adding design on the payment admin list

--- payment.php
+++ payment.php
@@ -8,9 +8,51 @@
     $table = $wpdb->prefix . 'payment';
     $rows = $wpdb->get_results("SELECT * FROM $table WHERE result='Y' ORDER BY id DESC");
 
-    di($rows);
-
     ?>
+
+    <table class="wp-list-table widefat fixed striped posts">
+        <thead>
+        <tr>
+            <th scope="col" id="id" class="manage-column">ID</th>
+            <th scope="col" id="user_account" class="manage-column">User Account</th>
+            <th scope="col" id="method" class="manage-column">Method</th>
+            <th scope="col" id="currency" class="manage-column">Currency</th>
+            <th scope="col" id="Amount" class="manage-column">Amount</th>
+            <th scope="col" id="stamp_create" class="manage-column">Stamp Create</th>
+            <th scope="col" id="date" class="manage-column">Stamp Finish</th>
+            <th scope="col" id="result" class="manage-column">Result</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <?php foreach ( $rows as $row ) { ?>
+        <tr>
+            <td scope="col" id="id" class="manage-column"><?php echo $row['user_id']?></td>
+            <td scope="col" id="user_account" class="manage-column"><?php echo $row['paygate_account']?></td>
+            <td scope="col" id="method" class="manage-column"><?php echo $row['method']?></td>
+            <td scope="col" id="currency" class="manage-column"><?php echo $row['currency']?></td>
+            <td scope="col" id="Amount" class="manage-column"><?php echo $row['amount']?></td>
+            <td scope="col" id="stamp_create" class="manage-column sortable desc"><?php echo $row['stamp_create']?></td>
+            <td scope="col" id="date" class="manage-column"><?php echo $row['stamp_finish']?></td>
+            <td scope="col" id="result" class="manage-column"><?php echo $row['result']?></td>
+        </tr>
+        <?php } ?>
+
+
+        </tbody>
+        <tfoot>
+        <tr>
+            <th scope="col" id="id" class="manage-column">ID</th>
+            <th scope="col" id="user_account" class="manage-column">User Account</th>
+            <th scope="col" id="method" class="manage-column">Method</th>
+            <th scope="col" id="currency" class="manage-column">Currency</th>
+            <th scope="col" id="Amount" class="manage-column">Amount</th>
+            <th scope="col" id="stamp_create" class="manage-column">Stamp Create</th>
+            <th scope="col" id="date" class="manage-column">Stamp Finish</th>
+            <th scope="col" id="result" class="manage-column">Result</th>
+        </tr>
+        </tfoot>
+    </table>
 
 
 </div>
