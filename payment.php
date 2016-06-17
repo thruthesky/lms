24d2b512142ca82f634c2bfd12219da9a802712b
lancelynyrd <you@example.com>
2016-06-17 23:05:56 +0800

    fix

--- payment.php
+++ payment.php
@@ -7,7 +7,7 @@
     global $wpdb;
     $table = $wpdb->prefix . 'payment';
     $rows = $wpdb->get_results("SELECT * FROM $table WHERE result='Y' ORDER BY id DESC");
-
+di($rows);
     ?>
 
     <table class="wp-list-table widefat fixed striped posts">
@@ -27,7 +27,7 @@
 
         <?php foreach ( $rows as $row ) { ?>
         <tr>
-            <td scope="col" id="id" class="manage-column"><?php echo $row['user_id']?></td>
+            <td scope="col" id="id" class="manage-column"><?php echo $row['id']?></td>
             <td scope="col" id="user_account" class="manage-column"><?php echo $row['paygate_account']?></td>
             <td scope="col" id="method" class="manage-column"><?php echo $row['method']?></td>
             <td scope="col" id="currency" class="manage-column"><?php echo $row['currency']?></td>
