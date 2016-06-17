5436a8bbf4575cb13ae5ca2f3821df63063b331a
JaeHo Song <thruthesky@gmail.com>
2016-06-18 00:10:33 +0900

    fix error on object

--- payment.php
+++ payment.php
@@ -7,7 +7,7 @@
     global $wpdb;
     $table = $wpdb->prefix . 'payment';
     $rows = $wpdb->get_results("SELECT * FROM $table WHERE result='Y' ORDER BY id DESC");
-
+//di($rows);
     ?>
 
     <table class="wp-list-table widefat fixed striped posts">
@@ -27,14 +27,14 @@
 
         <?php foreach ( $rows as $row ) { ?>
         <tr>
-            <td scope="col" id="id" class="manage-column"><?php echo $row['user_id']?></td>
-            <td scope="col" id="user_account" class="manage-column"><?php echo $row['paygate_account']?></td>
-            <td scope="col" id="method" class="manage-column"><?php echo $row['method']?></td>
-            <td scope="col" id="currency" class="manage-column"><?php echo $row['currency']?></td>
-            <td scope="col" id="Amount" class="manage-column"><?php echo $row['amount']?></td>
-            <td scope="col" id="stamp_create" class="manage-column sortable desc"><?php echo $row['stamp_create']?></td>
-            <td scope="col" id="date" class="manage-column"><?php echo $row['stamp_finish']?></td>
-            <td scope="col" id="result" class="manage-column"><?php echo $row['result']?></td>
+            <td scope="col" id="id" class="manage-column"><?php echo $row->id ?></td>
+            <td scope="col" id="user_account" class="manage-column"><?php echo $row->paygate_account ?></td>
+            <td scope="col" id="method" class="manage-column"><?php echo $row->method ?></td>
+            <td scope="col" id="currency" class="manage-column"><?php echo $row->currency ?></td>
+            <td scope="col" id="Amount" class="manage-column"><?php echo $row->amount ?></td>
+            <td scope="col" id="stamp_create" class="manage-column"><?php echo $row->stamp_create ?></td>
+            <td scope="col" id="date" class="manage-column"><?php echo $row->stamp_finish ?></td>
+            <td scope="col" id="result" class="manage-column"><?php echo $row->result ?></td>
         </tr>
         <?php } ?>
 
